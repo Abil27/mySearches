ff90174532396164abf7946303e3672a9db23e1c
Houssein abil <milameet@gmail.com>
2018-08-01 09:27:52 -0700

    user can view a searches

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello mySearches');
+const searches = [
+    {title: "link1",
+     description: "this is the firts link",
+     link: "http://link1.com" 
+    },
+
+    {title: "link2",
+    description: "this is the second link",
+    link: "http://link2.com" 
+    },
+
+    {title: "link3",
+    description: "this is the third link",
+    link: "http://link3.com" 
+    },
+
+    {title: "link4",
+    description: "this is the fourth link",
+    link: "http://link4.com" 
+    },
+]
+//route to see all the searches
+app.get('/searches', (req, res) => {
+    res.send(searches);
 })
+
+//
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
